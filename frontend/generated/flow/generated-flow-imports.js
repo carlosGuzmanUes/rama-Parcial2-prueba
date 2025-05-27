e30ad5df20743e89aeec2ce850fb1f40b8139a66
GF12016 Carlos Guzman <gf12016@ues.edu.sv>
2025-05-27 00:01:41 -0600

    Se actualiza front-end

--- frontend/generated/flow/generated-flow-imports.js
+++ frontend/generated/flow/generated-flow-imports.js
@@ -14,13 +14,13 @@ import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
 
 const loadOnDemand = (key) => {
   const pending = [];
-  if (key === 'd7ec4635d62f12d99abb1b1b9763dc65eff3b872607481de96e07688a911e71b') {
+  if (key === '164840db58f2873114272b78d79f4788ff177a30b8b410530b90a8a226edba07') {
     pending.push(import('./chunks/chunk-4e84c0dd41c912553be0261f217d80678981a33413bba4326e72841706a4b5a8.js'));
   }
-  if (key === '164840db58f2873114272b78d79f4788ff177a30b8b410530b90a8a226edba07') {
-    pending.push(import('./chunks/chunk-34d702aa1654846165a069b4e52b2816c804d781e2e3f936e5e2484b94a33500.js'));
-  }
   if (key === '9d5c9c3549706afb7064682f255b68a996a540823ba2298e26749d34db4fa719') {
+    pending.push(import('./chunks/chunk-4e84c0dd41c912553be0261f217d80678981a33413bba4326e72841706a4b5a8.js'));
+  }
+  if (key === 'd7ec4635d62f12d99abb1b1b9763dc65eff3b872607481de96e07688a911e71b') {
     pending.push(import('./chunks/chunk-4e84c0dd41c912553be0261f217d80678981a33413bba4326e72841706a4b5a8.js'));
   }
   return Promise.all(pending);
